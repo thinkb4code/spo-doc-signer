ad4c3f5b88ff1795af4583866ba44df54c10c81c
Manish Singh Rana <manish.manishr@gmail.com>
2024-11-04 19:35:43 -0800

    fix: image load issue

--- source/src/webparts/documentSigner/views/DocumentRender.tsx
+++ source/src/webparts/documentSigner/views/DocumentRender.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import styles from "../assets/css/DocumentRender.module.scss";
 import { IDocumentRenderProps, IPageContent } from "../model/DocumentRender.model";
+import * as _ from '@microsoft/sp-lodash-subset';
 
 const DocumentRender: React.FC<IDocumentRenderProps> = ({pages}) => {
     const pageHeight = 11 * 96; // 11 inches in pixels (96px per inch)
@@ -23,27 +24,43 @@ const DocumentRender: React.FC<IDocumentRenderProps> = ({pages}) => {
         return docPage;
     };
 
-    const setupPage = (pageContent: Document): void => {
+    // Helper function to create a promise that resolves when an image loads
+    // @ts-ignore
+    const imageLoadPromise = (img: HTMLImageElement) => new Promise((resolve, reject) => {
+        if (img.complete) {
+            resolve(true);
+        } else {
+            img.onload = resolve;
+            img.onerror = reject;
+        }
+    });
+
+    const setupPage = async (pageContent: Document): Promise<void> => {
         let currentPage: HTMLDivElement = newPage();
 
-        const fitContentInPage = (currentElem: Element, rootElem: Element | Node | undefined): void => {
-            if(rootElem) {
-                debugger;
-                currentElem = rootElem.appendChild(currentElem);
-            }
-
+        const fitContentInPage = async (currentElem: Element): Promise<void> => {
             currentPage.querySelector('.content')?.appendChild(currentElem);
+            
+            if(currentPage.outerHTML.indexOf('<img') > 0){
+                const images = currentPage.querySelectorAll('img');
+                const imgsLoaded = Array.from(images).map(imageLoadPromise);
+                await Promise.all(imgsLoaded);
+            }else {
+                await Promise.resolve();
+            }
 
             if(currentPage.scrollHeight > pageHeight) {
                 currentPage.querySelector('.content')?.removeChild(currentElem);
-                
                 if(currentElem.children.length > 0) {
-                    const parent = currentElem.cloneNode(false);
+                    //const parent = currentElem.cloneNode(false);
                     const childernElem = Array.prototype.slice.call(currentElem.children);
 
                     for(let i=0; i < childernElem.length; i++){
+                        debugger;
+                        let parent = currentElem.cloneNode(false) as Element;
                         const currElem = childernElem[i];
-                        fitContentInPage(currElem, parent);
+                        parent.appendChild(currElem);
+                        await fitContentInPage(currElem);
                     }
                 }else {
                     currentPage = newPage();
@@ -51,22 +68,25 @@ const DocumentRender: React.FC<IDocumentRenderProps> = ({pages}) => {
                 }
             }
         };
-
+        
+        //const parent = pageContent.documentElement.cloneNode(false);
         const childernElem = Array.prototype.slice.call(pageContent.body.children);
         for(let i=0; i < childernElem.length; i++){
             try{
                 const currElem = childernElem[i];
-                fitContentInPage(currElem, undefined);
+                await fitContentInPage(currElem);
             }catch{
-                //Do nothing
+                //Do nothing 
             }
         }
+
+        return Promise.resolve();
     };
 
     React.useEffect(() => {
         pages.map((p: IPageContent) => {
             const docContent = parser.parseFromString(p.Content, 'text/html');
-            setupPage(docContent)
+            setupPage(docContent);
         });
     });
 
